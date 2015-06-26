d0b0689f4621c39433137e12d7e9e87f556a38ef
matt price <matt.price@mandiant.com>
2015-06-26 16:07:17 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # snappycli
-CLI tool for compressing files using snappy
+CLI tool for compressing files using snappy.
+
+### Instal
+* Install Go
+* Checkout this repo to your Go workspace
+* Install snappycli `go install snappycli.go`
+
+### Usage
+```
+Usage of snappycli:
+  -input-file="": [REQUIRED] the file to be compressed
+  -output-file="": name of the file with the compressed contents - defaults to [input-file].gz
+```
+Note: using the `-h` flag will output this text.
---
+++ snappycli.go
@@ -0,0 +1,55 @@
+// CLI tool for compressing files with snappy.
+//
+// Please report any feature requests or bugs to https://github.com/suicidejack/snappycli/issues
+package main
+
+import (
+	"bufio"
+	"flag"
+	"log"
+	"os"
+
+	"github.com/golang/snappy/snappy"
+)
+
+var (
+	inputFilename  string
+	outputFilename string
+)
+
+func init() {
+	flag.StringVar(&inputFilename, "input-file", "", "[REQUIRED] the file to be compressed")
+	flag.StringVar(&outputFilename, "output-file", "", "name of the file with the compressed contents - defaults to [input-file].gz")
+}
+
+func main() {
+	validateFlags()
+
+	var err error
+	var in, out *os.File
+
+	if in, err = os.Open(inputFilename); err != nil {
+		log.Fatalf("unable to open input file %s: %s", inputFilename, err.Error())
+	}
+	bufin := bufio.NewReader(in)
+
+	if out, err = os.Create(outputFilename); err != nil {
+		log.Fatalf("unable to create output file %s: %s", outputFilename, err.Error())
+	}
+	writer := snappy.NewWriter(out)
+
+	bufin.WriteTo(writer)
+	in.Close()
+	out.Close()
+}
+
+func validateFlags() {
+	flag.Parse()
+
+	if inputFilename == "" {
+		log.Fatal("you must supply an input file")
+	}
+	if outputFilename == "" {
+		outputFilename = inputFilename + ".gz"
+	}
+}
